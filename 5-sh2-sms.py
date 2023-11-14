4133a922f15e12f06aa76b46b9ac4560212fb851
Toni <toni.giorgino@gmail.com>
2023-11-14 18:28:48 +0100

    uff

---
+++ 5-sh2-sms.py
@@ -0,0 +1,49 @@
+from htmd.ui import *
+import numpy as np
+from sklearn.cluster import * 
+from glob import *
+import matplotlib.pyplot as plt
+
+
+# See 1-precompute-Metrics where it is done
+
+contacts = MetricData(file="metrics/contacts.htmd")
+
+
+
+ticaproj = TICA(data = contacts, lag = 50)
+ticacontacts2 = ticaproj.project(ndim = 2)   # ndim = the number of TICA dimensions we want to project the data on
+
+for i,d in enumerate(ticacontacts2.dat):
+    plt.scatter(d[:,0], d[:,1], s=1, alpha=.1)
+plt.show()
+
+
+ticaproj = TICA(data = contacts, lag = 50)
+ticacontacts3 = ticaproj.project(ndim = 3)   # ndim = the number of TICA dimensions we want to project the data on
+
+fig = plt.figure(figsize = (10, 10))
+ax = plt.axes(projection ="3d")
+for i,d in enumerate(ticacontacts3.dat):
+    ax.scatter3D(d[:,0], d[:,1], d[:,2], s=1, alpha=.1)
+plt.show()
+
+
+
+# https://stackoverflow.com/questions/43187484/matplotlib-slow-3d-scatter-rotation
+
+import plotly.graph_objects as go
+
+ff=go.Figure()
+for i,d in enumerate(ticacontacts3.dat):
+    s3 = go.Scatter3d(
+        x=d[:,0],
+        y=d[:,1], 
+        z=d[:,2],
+        marker=go.scatter3d.Marker(size=1), 
+         # opacity=0.5, 
+        mode='markers'
+    )
+    ff.add_trace(s3)
+ff.show()
+
